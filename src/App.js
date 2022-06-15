7c420a556bf26cba995da966c4f693aefd7aaefa
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-15 14:39:03 +0100

    fetch api test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      await fetch(
+        `https://gist.githubusercontent.com/daviferreira/41238222ac31fe36348544ee1d4a9a5e/raw/5dc996407f6c9a6630bfcec56eee22d4bc54b518/employees.json`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    };
+    fetchEmployees();
+  }, []);
+
   return <div className="app"></div>;
 }
 
