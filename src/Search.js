9b7fa7eee2e36ce640bc663c0b447499d5a0ccc8
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-15 23:09:55 +0100

    style updates + state change for displaying list and search arrow

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
-import SearchResult from "./SearchResult";
 import SearchResults from "./SearchResults";
 import useGetEmployees from "./useGetEmployees";
 
 function Search() {
+  const [searchActive, setSearchActive] = useState(false);
   const employees = useGetEmployees();
+
+  const inputFocus = (e) => {
+    setSearchActive(true);
+  };
+  const inputBlur = (e) => {
+    setSearchActive(false);
+  };
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+  };
+
   console.log(employees);
   return (
     <div className="search">
       <div className="search__input">
         <form>
-          <input type="text" placeholder="Choose Manager" />
-          <button type="submit">Search</button>
+          <input
+            type="text"
+            onFocus={inputFocus}
+            onBlur={inputBlur}
+            placeholder="Choose Manager"
+          />
+          <button
+            className={searchActive ? "search__active" : ""}
+            onClick={submitSearch}
+            type="submit"
+          ></button>
         </form>
       </div>
-      <SearchResults />
+      {searchActive && <SearchResults />}
     </div>
   );
 }
