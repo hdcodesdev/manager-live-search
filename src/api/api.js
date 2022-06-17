bbe753694a79a0fef312af0bc3f6ac5281509ce8
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-17 20:35:14 +0100

    removed app test and old api file

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-export const getEmployeesFromApi = () => {
-  return fetch(
-    `https://gist.githubusercontent.com/daviferreira/41238222ac31fe36348544ee1d4a9a5e/raw/5dc996407f6c9a6630bfcec56eee22d4bc54b518/employees.json`
-  ).then((respnse) => {
-    if (respnse.status === 200) return respnse.json();
-    else throw new Error("Invalid response");
-  });
-};
