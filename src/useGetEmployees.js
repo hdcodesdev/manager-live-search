70426bde88c508d4e413bd6db700ed03edfc5080
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-17 17:59:06 +0100

    moving fetch api call to separate file

--- src/useGetEmployees.js
+++ src/useGetEmployees.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { getEmployeesFromApi } from "./api/api";
 
 function useGetEmployees() {
   const [employees, setEmployees] = useState([]);
   useEffect(() => {
     const fetchEmployees = async () => {
-      await fetch(
-        `https://gist.githubusercontent.com/daviferreira/41238222ac31fe36348544ee1d4a9a5e/raw/5dc996407f6c9a6630bfcec56eee22d4bc54b518/employees.json`
-      )
-        .then((res) => res.json())
+      await getEmployeesFromApi()
         .then((responseData) => {
           const baseData = responseData.data;
           const includedData = responseData.included;
