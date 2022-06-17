2f343b035f3c1100b736cd6cce2b85cab4ca4b98
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-17 13:25:45 +0100

    scroll event when item is active

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./SearchResult.css";
 
 function SearchResult({ initials, name, email, onEmployeeClick, isActive }) {
+  const itemRef = useRef();
+
+  useEffect(() => {
+    if (isActive && itemRef.current) {
+      itemRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [isActive]);
+
   return (
     <div
       className="searchResult"
       aria-selected={isActive ? "true" : "false"}
       onClick={onEmployeeClick}
+      ref={itemRef}
     >
       <div className="searchResult__avatar">
         <p>{initials}</p>
