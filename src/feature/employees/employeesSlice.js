27f599aebf00e524073e391651872aa3c3d7ef83
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-18 22:08:34 +0100

    updating api + employee slice

--- src/feature/employees/employeesSlice.js
+++ src/feature/employees/employeesSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getEmployeesFromApi } from "../../api";
 
 const initials = {
@@ -8,33 +8,15 @@ const initials = {
 };
 
 export const fetchEmployees = createAsyncThunk(
-  "employees/fetchEmployees",
+  "data/fetchEmployees",
   async () => {
-    const response = await getEmployeesFromApi()
-      .then((responseData) => {
-        const baseData = responseData.data;
-        const includedData = responseData.included;
-
-        const employeesList = baseData.map((manager) => {
-          const email = includedData.filter(
-            (account) => account.id === manager.relationships.account.data.id
-          );
-
-          return {
-            id: manager.id,
-            name: manager.attributes.name,
-            email: email[0].attributes.email,
-            initials:
-              manager.attributes.firstName[0] + manager.attributes.lastName[0],
-          };
-        });
-
-        setEmployees(employeesList);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
+    const response = await getEmployeesFromApi();
     return response;
   }
 );
+
+export const employeesSlice = createSlice({
+  name: "employees",
+  initialState,
+  reducers: {},
+});
